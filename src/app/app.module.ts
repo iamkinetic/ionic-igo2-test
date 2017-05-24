b47a777a01e5d30de3eb0be5250be9a72ccccc02
frederic.catellier <frederic.catellier@cauca.ca>
2017-05-24 11:25:38 -0400

    (chore) Add igo2 and use the IgoMapComponent in app.html.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import {IgoModule} from 'igo2';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   imports: [
+    IgoModule.forRoot(),
     BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
